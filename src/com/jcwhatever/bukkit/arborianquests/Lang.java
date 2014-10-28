1ada2d0c7c1bd40af97e0aa4ab5fdef2385bcd4b
jcThePants <jcthepants@gmail.com>
2014-10-27 17:53:49 -0700

    Implement localization and fix existing localization to match changes

--- src/com/jcwhatever/bukkit/arborianquests/Lang.java
+++ src/com/jcwhatever/bukkit/arborianquests/Lang.java
@@ -24,21 +24,14 @@
 
 package com.jcwhatever.bukkit.arborianquests;
 
-import com.jcwhatever.bukkit.generic.language.LanguageManager;
 import com.jcwhatever.bukkit.generic.language.Localized;
 
-/**
- * Created by John on 10/13/2014.
- */
 public class Lang {
 
     private Lang() {}
 
-    private static LanguageManager _languageManager = new LanguageManager();
-
-
     @Localized
     public static String get(String text, Object... params) {
-        return _languageManager.get(ArborianQuests.getInstance(), text, params);
+        return ArborianQuests.getInstance().getLanguageManager().get(text, params);
     }
 }
