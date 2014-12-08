a422d37adef4a8d37f2565d381f250c85262a84b
JCThePants <jcthepants@gmail.com>
2014-12-08 10:53:56 -0800

    fix Msg to use GenericsLibs new messenger system

--- src/com/jcwhatever/bukkit/arborianquests/Msg.java
+++ src/com/jcwhatever/bukkit/arborianquests/Msg.java
@@ -24,9 +24,10 @@
 
 package com.jcwhatever.bukkit.arborianquests;
 
-import com.jcwhatever.bukkit.generic.messaging.Messenger;
 import com.jcwhatever.bukkit.generic.messaging.ChatPaginator;
+import com.jcwhatever.bukkit.generic.messaging.IMessenger;
 import com.jcwhatever.bukkit.generic.utils.text.TextUtils;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -38,45 +39,50 @@ public class Msg {
     private Msg() {}
 
     public static void tell(CommandSender sender, String message, Object...params) {
-        Messenger.tell(ArborianQuests.getPlugin(), sender, message, params);
+        msg().tell(sender, message, params);
     }
 
     public static void tell(Player p, String message, Object...params) {
-        Messenger.tell(ArborianQuests.getPlugin(), p, message, params);
+        msg().tell(p, message, params);
     }
 
     public static void tellImportant(UUID playerId, String context, String message, Object...params) {
-        Messenger.tellImportant(ArborianQuests.getPlugin(), playerId, context, message, params);
+        msg().tellImportant(playerId, context, message, params);
     }
 
     public static void info(String message, Object...params) {
-        Messenger.info(ArborianQuests.getPlugin(), message, params);
+        msg().info(message, params);
     }
 
     public static void debug(String message, Object...params) {
-        //if (!Plugin.getInstance().isDebugging())
-        //	return;
-        Messenger.debug(ArborianQuests.getPlugin(), message, params);
+        if (!ArborianQuests.getPlugin().isDebugging())
+        	return;
+
+        msg().debug(message, params);
     }
 
     public static void warning(String message, Object...params) {
-        Messenger.warning(ArborianQuests.getPlugin(), message, params);
+        msg().warning(message, params);
     }
 
     public static void severe(String message, Object...params) {
-        Messenger.severe(ArborianQuests.getPlugin(), message, params);
+        msg().severe(message, params);
     }
 
     public static void broadcast(String message, Object...params) {
-        Messenger.broadcast(ArborianQuests.getPlugin(), message, params);
+        msg().broadcast(message, params);
     }
 
     public static void broadcast(String message, Collection<Player> exclude, Object...params) {
-        Messenger.broadcast(ArborianQuests.getPlugin(), message, exclude, params);
+        msg().broadcast(message, exclude, params);
     }
 
     public static ChatPaginator getPaginator(String title, Object...params) {
         return new ChatPaginator(ArborianQuests.getPlugin(), 6, TextUtils.format(title, params));
+    }
+
+    private static IMessenger msg() {
+        return ArborianQuests.getPlugin().getMessenger();
     }
 
 }
